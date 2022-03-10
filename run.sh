386b54ad93bb1f03ec5009a8a49bf58a7f080268
howset <howsetya@googlemail.com>
2022-03-10 15:32:43 +0100

    Minor fix

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ k=0
 for Dir in $HOME/workspace/Bioimage2/Images/*/*/subset/
 do
    #echo $Dir ${set[k]}
-   python3 cell_count.py $Dir $set[k] 
+   python3 cell_count.py $Dir ${set[k]}
    k=$(($k + 1))
 done
