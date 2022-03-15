a5d6b7a53ea85d78645b388ee9944d5e779df557
howset <howsetya@googlemail.com>
2022-03-15 16:42:45 +0100

    Edit run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 declare -a set=('ch1' 'ch2' 'tr1' 'tr2')
 k=0
 
-for Dir in $HOME/workspace/Bioimage2/Images/*/*/subset/
+#for Dir in $HOME/Bioimage/Images/*/*/ #server
+for Dir in $HOME/workspace/Bioimage2/Images/*/*/subset/ #h laptop
 do
    #echo $Dir ${set[k]}
    python3 cell_count.py $Dir ${set[k]}
